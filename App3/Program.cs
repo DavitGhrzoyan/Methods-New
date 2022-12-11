a16a1f56ef2cdcdb78e6875d07305a749ae8255a
Davit <ghrzoyandavit@gmail.com>
2022-12-11 12:52:22 +0400

    Add project files.

---
+++ App3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace App3
+{
+    class Program
+    {
+
+        static void PrintLine(string symbol, uint symbolCount)
+        {
+            for (int i = 0; i < symbolCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a symbol:");
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine("Enter a quantity of symbol:");
+            uint symbolCount = uint.Parse(Console.ReadLine());
+
+
+            PrintLine(symbol, symbolCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Char/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Char
+{
+    class Program
+    {
+        static void PrintLine(char symbol, uint symbolCount)
+        {
+            for (int i = 0; i < symbolCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character:");
+            char symbol = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\nEnter a quantity of character:");
+            uint symbolCount = uint.Parse(Console.ReadLine());
+
+
+            PrintLine(symbol, symbolCount);
+            Console.ReadLine();
+        }
+    }
+}
